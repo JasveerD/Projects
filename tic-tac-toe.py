d2aaa53722f5a3075e897d1a9fbbce652752bbe2
Jasveer_D <93256850+Lil-Buddha@users.noreply.github.com>
2023-03-30 16:27:03 +0530

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,12 @@
-board = [['-','-','-'], ['-','-','-'], ['-','-','-']]
+board = [['X','O','X'], ['X','O','-'], ['O','-','O']]
 turn = 0
 
 def showBoard():
     print(board[0][0] + '  |  ' + board[0][1] + '  |  ' + board[0][2])
-    print('--------------')
+    print('---+-----+---')
     print(board[1][0] + '  |  ' + board[1][1] + '  |  ' + board[1][2])
-    print('--------------')
+    print('---+-----+---')
     print(board[2][0] + '  |  ' + board[2][1] + '  |  ' + board[2][2])
-
 
 def turnInput():
     row = input("Enter row: ")
@@ -52,7 +51,7 @@ def rowWinCheck(gameBoard):
 
 def colWinCheck(gameBoard):
     for i in range(len(gameBoard)):
-        if gameBoard[0][i] == gameBoard[1][i] == gameBoard[0][i] == 'X':
+        if gameBoard[0][i] == gameBoard[1][i] == gameBoard[2][i] == 'X':
             print('X wins!')
             exit()
         elif gameBoard[0][i] == gameBoard[1][i] == gameBoard[2][i] == 'O':
@@ -81,7 +80,18 @@ def diagWinCheck(gameBoard):
     else:
         pass
 
+def drawCheck(gameBoard):
+    count = 0
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 'X' or board[i][j] == 'O':
+                count = count + 1
+    if count == 9:
+        print('Draw!')
+        exit()
+
 def winCheck(gameBoard):
+    drawCheck(gameBoard)
     rowWinCheck(gameBoard)
     colWinCheck(gameBoard)
     diagWinCheck(gameBoard)
